5dbb1eee6052b2783b1ae55c5ddecb3662956f8d
Joss <joss.raine@gmail.com>
2025-12-05 15:03:53 +1100

    feat: adjusted starRating UI feature from previous project for mobile first design

--- climbing-tracker-mobile/src/utilities/customHooks/useAttempts.js
+++ climbing-tracker-mobile/src/utilities/customHooks/useAttempts.js
@@ -1,8 +1,19 @@
-import { useQuery } from "@tanstack/react-query";
-import { getAttempts } from "../services/apiServices";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { createAttempt, getAttempts } from "../services/apiServices";
 
-export const useAllAttempts = () => 
-    useQuery({
-        queryKey: ["user-attempts"],
-        queryFn: getAttempts
-    })
+export const useAllAttempts = () =>
+  useQuery({
+    queryKey: ["user-attempts"],
+    queryFn: getAttempts,
+  });
+
+export const useAddAttempt = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: createAttempt,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["user-attempts"] });
+    },
+  });
+};
